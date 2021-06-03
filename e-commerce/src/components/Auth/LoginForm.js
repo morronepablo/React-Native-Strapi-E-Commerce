b3ea5ff7d6561a20440330fbabd13a7e3f69fd74
morronepablo <morronepablo@gmail.com>
2021-06-03 14:51:22 -0300

    Realizando el login contra Strapi

--- e-commerce/src/components/Auth/LoginForm.js
+++ e-commerce/src/components/Auth/LoginForm.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
 import { TextInput, Button } from "react-native-paper";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import Toast from 'react-native-root-toast';
+import { loginApi } from "../../api/user";
 import { formStyle } from "../../styles";
 
 export default function LoginForm(props) {
     const { changeForm } = props;
+    const [loading, setLoading] = useState(false);
 
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: Yup.object(validationSchema()),
         onSubmit: async (formData) => {
-            //setLoading(true);
+            setLoading(true);
             try {
-                console.log("Formulario enviado!");
-                console.log(formData);
-                //await registerApi(formData);
+                const response = await loginApi(formData);
+                if (response.statusCode) throw "Error en el usuario o contraseña";
+                console.log(response);
                 //changeForm();
             } catch (error) {
-                setLoading(false);
-                Toast.show("Error al registrar el usuario", {
+                //console.log("error");
+                Toast.show(error, {
                     position: Toast.positions.CENTER,
                 });
+                setLoading(false);
             } 
         },
     });
@@ -42,11 +46,13 @@ export default function LoginForm(props) {
                 onChangeText={(text) => formik.setFieldValue("password", text)}
                 value={formik.values.password}
                 error={formik.errors.password}
+                secureTextEntry
             />
             <Button 
                 mode="contained" 
                 style={formStyle.btnSucces}
                 onPress={formik.handleSubmit}
+                loading={loading}
             >
                 Entrar
             </Button>
