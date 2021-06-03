26a0c9b6a9584f42bc7fe0804fc8a168667afe4b
morronepablo <morronepablo@gmail.com>
2021-06-03 13:52:54 -0300

    Controlando y validando datos del formulario de login

--- e-commerce/src/components/Auth/LoginForm.js
+++ e-commerce/src/components/Auth/LoginForm.js
@@ -1,24 +1,52 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { formStyle } from "../../styles";
 
 export default function LoginForm(props) {
     const { changeForm } = props;
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: async (formData) => {
+            //setLoading(true);
+            try {
+                console.log("Formulario enviado!");
+                console.log(formData);
+                //await registerApi(formData);
+                //changeForm();
+            } catch (error) {
+                setLoading(false);
+                Toast.show("Error al registrar el usuario", {
+                    position: Toast.positions.CENTER,
+                });
+            } 
+        },
+    });
 
     return (
         <View>
             <TextInput 
                 label="Email o Username"
                 style={formStyle.input}
+                onChangeText={(text) => formik.setFieldValue("identifier", text)}
+                value={formik.values.identifier}
+                error={formik.errors.identifier}
             />
             <TextInput 
                 label="Contraseña"
                 style={formStyle.input}
+                onChangeText={(text) => formik.setFieldValue("password", text)}
+                value={formik.values.password}
+                error={formik.errors.password}
             />
             <Button 
                 mode="contained" 
                 style={formStyle.btnSucces}
+                onPress={formik.handleSubmit}
             >
                 Entrar
             </Button>
@@ -32,4 +60,18 @@ export default function LoginForm(props) {
             </Button>
         </View>
     );
+}
+
+function initialValues() {
+    return {
+        identifier: "",
+        password: ""
+    }
+}
+
+function validationSchema() {
+    return {
+        identifier: Yup.string().required(true),
+        password: Yup.string().required(true),
+    };
 }
