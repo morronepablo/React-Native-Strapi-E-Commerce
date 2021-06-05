b7baf3da7d5366c6aece682032941b27823f4198
morronepablo <morronepablo@gmail.com>
2021-06-05 02:32:34 -0300

    Integrando formik y yup en el formulario

--- e-commerce/src/screens/Account/ChangeName.js
+++ e-commerce/src/screens/Account/ChangeName.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { formStyle } from "../../styles";
 
 export default function ChangeName() {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formData) => {
+            console.log("Formulario enviado...");
+            console.log(formData);
+        }
+    });
+
     return (
         <View style={styles.container}>
-            <TextInput 
+            <TextInput
                 label="Nombre"
                 style={formStyle.input}
+                onChangeText={(text) => formik.setFieldValue("name", text)}
+                value={formik.values.name}
+                error={formik.errors.name}
             />
             <TextInput 
                 label="Apellidos"
                 style={formStyle.input}
+                onChangeText={(text) => formik.setFieldValue("lastname", text)}
+                value={formik.values.lastname}
+                error={formik.errors.lastname}
             />
             <Button
                 mode="contained"
                 style={formStyle.btnSucces}
-                onPress={() => console.log("Formulario enviado...")}
+                onPress={formik.handleSubmit}
             >
                 cambiar nombre y apellidos
             </Button>
@@ -30,3 +48,17 @@ const styles = StyleSheet.create({
         padding: 20,
     },
 });
+
+function initialValues() {
+    return {
+      name: "",
+      lastname: "",
+    };
+}
+  
+function validationSchema() {
+    return {
+        name: Yup.string().required(true),
+        lastname: Yup.string().required(true),
+    };
+}
