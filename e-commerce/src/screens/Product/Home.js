35cd87bad36e55883885afbd89a973d9f9ea9084
morronepablo <morronepablo@gmail.com>
2021-06-06 00:34:09 -0300

    Añadiendo StatusBar y Search en la home

--- e-commerce/src/screens/Product/Home.js
+++ e-commerce/src/screens/Product/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import StatusBar from "../../components/StatusBar";
+import Search from "../../components/Search";
+
+import colors from "../../styles/colors";
 
 export default function Home() {
     return (
-        <View style={styles.container}>
-            <Text>Estamos en la HOME</Text>
-        </View>
+        <>
+            <StatusBar backgroundColor={colors.bgDark} barStyle="light-content" />
+            <Search />
+            <ScrollView>
+                {/* Banner */}
+                {/* NewProducts */}
+            </ScrollView>
+        </>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
