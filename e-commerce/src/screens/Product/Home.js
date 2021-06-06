65010ac5fa3c07b26227934a64210b98e2d7171e
morronepablo <morronepablo@gmail.com>
2021-06-06 16:51:17 -0300

    Obteniendo los ultimos productos añadidos

--- e-commerce/src/screens/Product/Home.js
+++ e-commerce/src/screens/Product/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import StatusBar from "../../components/StatusBar";
 import Search from "../../components/Search";
-
+import NewProducts from "../../components/Home/NewProducts";
 import colors from "../../styles/colors";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
             <Search />
             <ScrollView>
                 {/* Banner */}
-                {/* NewProducts */}
+                <NewProducts />
             </ScrollView>
         </>
     );
